12ec479db7782a81ad10f5d9bdeae2fc4691ba3c
clement <clementadebola54@gmail.com>
2024-08-01 10:45:54 +0100

    new

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromApi";
+
+const ChannelDetail = () => {
+  return (
+    <div>ChannelDetail</div>
+  )
+}
+
+export default ChannelDetail
